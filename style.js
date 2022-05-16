85666c0515d97a88005dbf4a67d9ec0e0d825f63
Hassanibrahim0617 <103922175+Hassanibrahim0617@users.noreply.github.com>
2022-05-16 16:35:21 +0800

    Add files via upload

---
+++ style.js
@@ -0,0 +1,24 @@
+const navSlide = () => {
+    const hamburger = document.querySelector(".hamburger");
+    const nav = document.querySelector(".href");
+    const href = document.querySelectorAll(".href ul li a");
+
+    hamburger.addEventlistener()
+        "click", () => {
+            // toggle
+            nav.classList.toggle{ "nav-active" };
+
+            // comment line
+            navLinks.forEach{(link,index)} => {
+                    if (link, style, animation) {
+                        link, style, anumation = '';
+                    } else {
+                        link, style, animation = 'navLinkFade 0.5s ease forwards'
+                    }
+                }
+            }
+        }
+    };
+
+}
+navSlide{ };
