ba05b3550f950f756500924ef46009c1b97b369c
Hassanibrahim0617 <103922175+Hassanibrahim0617@users.noreply.github.com>
2022-05-17 01:06:50 +0800

    Add files via upload

--- style.js
+++ style.js
@@ -1,24 +1,62 @@
-const navSlide = () => {
-    const hamburger = document.querySelector(".hamburger");
-    const nav = document.querySelector(".href");
-    const href = document.querySelectorAll(".href ul li a");
+// $(document).ready(funtion(){
+//    $(`#hamburger`).click(function(){
+//         $(`ul`).togleclass(`show`)
+//     });
+// });
 
-    hamburger.addEventlistener()
-        "click", () => {
-            // toggle
-            nav.classList.toggle{ "nav-active" };
 
-            // comment line
-            navLinks.forEach{(link,index)} => {
-                    if (link, style, animation) {
-                        link, style, anumation = '';
-                    } else {
-                        link, style, animation = 'navLinkFade 0.5s ease forwards'
-                    }
-                }
-            }
-        }
-    };
 
-}
-navSlide{ };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const navSlide = () => {
+//     const hamburger = document.querySelector(".hamburger");
+//     const nav = document.querySelector(".href");
+//     const href = document.querySelectorAll(".href ul li a");
+
+//     hamburger.addEventlistener()
+//         "click", () => {
+//             // toggle
+//             nav.classList.toggle{ "nav-active" };
+
+//             // comment line
+//             navLinks.forEach{(link,index)} => {
+//                     if (link, style, animation) {
+//                         link, style, anumation = '';
+//                     } else {
+//                         link, style, animation = 'navLinkFade 0.5s ease forwards'
+//                     }
+//                 }
+//             }
+//         }
+//     };
+
+// }
+// navSlide{ };
